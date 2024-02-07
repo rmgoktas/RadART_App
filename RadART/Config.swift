dc7b265fc4656820ca423bc95c28ba05f842304f
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2024-02-07 21:22:31 +0300

    Initial Commit

---
+++ RadART/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  RadART
+//
+//  Created by R. Metehan GÖKTAŞ on 7.02.2024.
+//
+
+import Foundation
+
+struct Config {
+    static let serverURL = "http://35.156.39.139:5000/predict"
+}
